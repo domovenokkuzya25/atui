2a3c30488008d504fbc5505720812029a9f437ab
domovenokkuzya25 <denisshcheglov@mail.ru>
2026-02-11 16:39:31 +0500

    Фикстуры Pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
-from playwright.sync_api import sync_playwright, Page
+from playwright.sync_api import sync_playwright, Page, Playwright
 
 @pytest.fixture
-def chromium_page() -> Page:
+def chromium_page(playwright: Playwright) -> Page:
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
         yield browser.new_page()
